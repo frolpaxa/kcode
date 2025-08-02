5526ce71091fe308ac6c55c3fbf4b91990eac86b
frolpaxa <frolpaxa@frolpaxas-MacBook-Air.local>
2025-08-02 17:28:54 +0300

    version up

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="kcode",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     author="Pavel Frolov",
     author_email="pfrol007@gmail.com",
